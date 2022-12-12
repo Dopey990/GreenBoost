b1bce907b5a06c16fcf1bf75e5e41b6ec658f791
Robin <robin.dautel@gmail.com>
2022-12-12 11:07:24 +0100

    rename package Impl to impl and add @repository to impl files

--- backend/src/main/java/com/greenboost_team/backend/controller/TownController.java
+++ backend/src/main/java/com/greenboost_team/backend/controller/TownController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
