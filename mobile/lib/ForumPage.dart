39f4fe523b4d087d5571f798ef07796eaefb10d2
Naïm Gallouj <37388224+Squ3D@users.noreply.github.com>
2023-01-05 11:38:46 +0100

    adding forum page front

---
+++ mobile/lib/ForumPage.dart
@@ -0,0 +1,62 @@
+import 'package:GreenBoost/profilePage.dart';
+import 'package:flutter/material.dart';
+import '/components/menu.dart';
+
+class ForumPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forum"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Rechercher dans le forum...",
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  title: Text("Titre du sujet 1"),
+                  subtitle: Text("Dernier message posté il y a 5 minutes"),
+                  onTap: () {
+                    // Navigate to the topic page
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  title: Text("Titre du sujet 2"),
+                  subtitle: Text("Dernier message posté il y a 2 heures"),
+                  onTap: () {
+                    // Navigate to the topic page
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  title: Text("Titre du sujet 3"),
+                  subtitle: Text("Dernier message posté hier"),
+                  onTap: () {
+                    // Navigate to the topic page
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // Navigate to the create topic page
+        },
+      ),
+    );
+  }
+}
