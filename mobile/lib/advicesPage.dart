60b6b3494c40c1b2803b8624e33ee39abe27de67
lNeo0 <maxime.consigny@gmail.com>
2023-01-05 12:28:14 +0100

    ajout page conseil advicesPageElectricity

--- mobile/lib/advicesPage.dart
+++ mobile/lib/advicesPage.dart
@@ -1,3 +1,4 @@
+import 'package:GreenBoost/advicesPageElectricity.dart';
 import 'package:flutter/material.dart';
 
 import 'components/menu.dart';
@@ -6,66 +7,83 @@ class AdvicesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Home'),
-        ),
-        drawer: const Menu(),
-        body: 
-          Container(
-            child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //icon of home
-                  IconButton(
-                    onPressed: () => {
-                      Navigator.push(context,MaterialPageRoute(builder: (context) => AdvicesPage()),)
-                    },
-                    color: Colors.blue,
-                    icon: const Icon(Icons.home, size: 40),
-                  ),
-                ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home'),
+      ),
+      drawer: const Menu(),
+      body: Container(
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //icon of home
+              IconButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdvicesPage()),
+                  )
+                },
+                color: Colors.blue,
+                icon: const Icon(Icons.home, size: 40),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //icon of home
-                  IconButton(
-                    onPressed: () => {
-                      Navigator.push(context,MaterialPageRoute(builder: (context) => AdvicesPage()),)
-                    },
-                    color: Colors.blue,
-                    icon: const Icon(Icons.home, size: 40),
-                  ),
-                  IconButton(
-                    onPressed: () => {
-                      Navigator.push(context,MaterialPageRoute(builder: (context) => AdvicesPage()),)
-                    },
-                    color: Colors.blue,
-                    icon: const Icon(Icons.home, size: 40),
-                  ),
-                  IconButton(
-                    onPressed: () => {
-                      Navigator.push(context,MaterialPageRoute(builder: (context) => AdvicesPage()),)
-                    },
-                    color: Colors.blue,
-                    icon: const Icon(Icons.home, size: 40),
-                  ),
-              ],),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                IconButton(
-                  onPressed: () => {
-                    Navigator.push(context,MaterialPageRoute(builder: (context) => AdvicesPage()),)
-                  },
-                  color: Colors.blue,
-                  icon: const Icon(Icons.home, size: 40),
-                ),
-              ],),
-            ]
-          ),),
-        );
-}
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //icon of home
+              IconButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdvicesPage()),
+                  )
+                },
+                color: Colors.blue,
+                icon: const Icon(Icons.home, size: 40),
+              ),
+              IconButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdvicesPage()),
+                  )
+                },
+                color: Colors.blue,
+                icon: const Icon(Icons.home, size: 40),
+              ),
+              IconButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdvicesPage()),
+                  )
+                },
+                color: Colors.blue,
+                icon: const Icon(Icons.home, size: 40),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => advicesPageElectricity()),
+                  )
+                },
+                color: Colors.blue,
+                icon: const Icon(Icons.home, size: 40),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
 }
