439406fd3ca642ed02915e770d376e508be4a458
Robin <robin.dautel@gmail.com>
2023-01-05 12:33:03 +0100

    modif on product to have a search by category and add houseProductController

--- backend/src/main/java/com/greenboost_team/backend/dto/ProductDto.java
+++ backend/src/main/java/com/greenboost_team/backend/dto/ProductDto.java
@@ -1,4 +1,21 @@
 package com.greenboost_team.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class ProductDto {
+    private String id;
+    private String orgVerificationStatus;
+    private Integer onMarketStartDateTS;
+    private String energyClass;
+    private Integer onMarketFirstStartDateTS;
+    private String supplierOrTrademark;
 }
