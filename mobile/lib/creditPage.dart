33cc249338a0ccc5ad7a400223504fd92f900391
josuvidr <59601350+josuvidr@users.noreply.github.com>
2023-01-18 21:51:31 +0100

    partenaires

--- mobile/lib/creditPage.dart
+++ mobile/lib/creditPage.dart
@@ -42,12 +42,36 @@ class CreditPage extends StatelessWidget {
             SizedBox(height: 16.0),
             Text('Nos associations partenaires:',
                 style: TextStyle(fontWeight: FontWeight.bold)),
-            Image.asset('assets/home/eau-vert.png',
+            Image.asset('assets/img/insa.jpg',
                 height: MediaQuery.of(context).size.width / 6,
                 width: MediaQuery.of(context).size.width / 6),
             Center(
               child: Text(
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                "Lorem Ipsum is simply dummy text ",
+              ),
+            ),
+            Image.asset('assets/img/renault.png',
+                height: MediaQuery.of(context).size.width / 6,
+                width: MediaQuery.of(context).size.width / 6),
+            Center(
+              child: Text(
+                "Lorem Ipsum is simply dummy text",
+              ),
+            ),
+            Image.asset('assets/img/edf.png',
+                height: MediaQuery.of(context).size.width / 6,
+                width: MediaQuery.of(context).size.width / 6),
+            Center(
+              child: Text(
+                "Lorem Ipsum is simply dummy text",
+              ),
+            ),
+             Image.asset('assets/img/decathlon.png',
+                height: MediaQuery.of(context).size.width / 6,
+                width: MediaQuery.of(context).size.width / 6),
+            Center(
+              child: Text(
+                "Lorem Ipsum is simply dummy text",
               ),
             ),
             Text(
