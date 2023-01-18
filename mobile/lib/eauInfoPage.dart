173cb25484ff4289e98d4c6028fddd25bf5fbfa5
Naïm Gallouj <37388224+Squ3D@users.noreply.github.com>
2023-01-18 11:36:45 +0100

    Add commit page eau
    test

--- mobile/lib/eauInfoPage.dart
+++ mobile/lib/eauInfoPage.dart
@@ -5,6 +5,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'components/advicesViewer.dart';
 import 'components/menu.dart';
 import 'components/pricesForecastChart.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class EauInfoPage extends StatefulWidget {
   const EauInfoPage({super.key});
@@ -14,7 +15,18 @@ class EauInfoPage extends StatefulWidget {
 }
 
 class EauInfoState extends State<EauInfoPage> {
+  //M'insulte pas Thomas =
+  final List<String> images = [
+    "https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg",
+    "https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg",
+    "https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg",
+    "https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg",
+    "https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg",
+    "https://www.example.com/image6.jpg",
+  ];
   @override
+  int _currentPage = 0;
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -30,16 +42,33 @@ class EauInfoState extends State<EauInfoPage> {
             Image.asset('assets/home/eau-vert.png',
                 height: MediaQuery.of(context).size.width / 6,
                 width: MediaQuery.of(context).size.width / 6),
-            const PricesForecastWidget(
-              chartTitle: "Consomation d'eau national",
-              apiUrl: "http://localhost:8080/prices/eau",
+            SizedBox(height: 20.0),
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: [
+                'https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg',
+                'https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg',
+                'https://www.un.org/africarenewal/sites/www.un.org.africarenewal/files/12-15-2015WashUNICEF.jpg',
+              ].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 185, 177, 188)),
+                      child: Image.network(i),
+                    );
+                  },
+                );
+              }).toList(),
             ),
-            ProductsActivitiesToolboxWidget(),
             const Padding(padding: EdgeInsets.only(top: 20)),
             AdvicesViewerWidget(
               apiUrl:
-                  "http://localhost:8080/advices/getByCategory?category=eau&language=FR",
-            )
+                  "http://localhost:8080/advices/getByCategory?category=water&language=FR",
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20)),
           ],
         ))));
   }
